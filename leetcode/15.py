ceaf3a508a48349d79c359d09af92b7322d6f56c
yangyang <yangyang@ishansong.com>
2019-12-23 16:23:30 +0800

    add some algorithm

--- leetcode/15.py
+++ leetcode/15.py
@@ -39,23 +39,41 @@ class Solution(object):
         """
         n = len(nums)
         res_list = []
-        map = {}
+        # 若数组nums为空或长度小于3直接返回
+        if not nums or n < 3:
+            return res_list
+        # 对数组进行排序
+        nums.sort()
         for i in range(n):
-            map[nums[i]] = i
-        print(map)
-        for j in range(n):
-            for k in range(j+1,n):
-                res = - (nums[j] + nums[k])
-                if map.get(res) is not None and res != nums[j] != nums[k]:
-                    res_list.append([res,nums[j],nums[k]])
-        print(res_list)
-
-        for res in res_list:
-            pass
-
+            # 对于重复元素直接跳过
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            else:
+                # 左指针
+                l = i + 1
+                # 右指针
+                r = n - 1
+                while l < r:
+                    # 当 nums[i] + nums[l] + nums[r] == 0 符合条件，左右指针移动
+                    if nums[i] + nums[l] + nums[r] == 0:
+                        res_list.append([nums[i], nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        # 判断左界和右界和下一位是否有重复，如果有各移动一位
+                        while l < n - 1 and nums[l] == nums[l - 1]:
+                            l += 1
+                        while r > 0 and nums[r] == nums[r + 1]:
+                            r -= 1
+                    # 若和小于0，说明nums[l]太小，l+1 右移
+                    elif nums[i] + nums[l] + nums[r] < 0:
+                        l += 1
+                    # 若和大于0，说明nums[r]太大，r-1左移
+                    else:
+                        r -= 1
+        return res_list
 
 
 if __name__ == '__main__':
-    nums = [-1, 0, 1, 2, -1, -4]
+    nums = [1, -1, -1, 0]
     slt = Solution()
-    slt.three_sum2(nums)
+    print(slt.three_sum2(nums))
