e2be939e6f5ea541a714c17dc34e16f57d9cb08c
yangyang <yangyang@ishansong.com>
2020-09-03 16:45:17 +0800

    add

--- algorithm/tree/binary_search_tree.py
+++ algorithm/tree/binary_search_tree.py
@@ -1,13 +1,14 @@
 # -*- coding: utf8 -*-
 
 
-#实现二叉查找树
+# 实现二叉查找树
 
 class TreeNode(object):
     """
     树的节点
     """
-    def __init__(self, data=None, left_node=None, right_node=None,parent_node=None):
+
+    def __init__(self, data=None, left_node=None, right_node=None, parent_node=None):
         self._data = data  # 存存储的数据
         self.left_node = left_node  # 左子节点
         self.right_node = right_node  # 右子节点
@@ -18,39 +19,42 @@ class BinarySearchTree(object):
     """
     实现二叉查找树
     """
+
     def __init__(self):
         self.root = None
 
-    def add(self,data):
+    def add(self, data):
         """
         增加节点
         :param data:
         :return:
         """
         new_node = TreeNode(data)
-        #如果树为空
+        # 如果树为空
         if self.root is None:
             self.root = new_node
         else:
             tree_node = self.root
             while tree_node is not None:
-                #如果要插入的值大于等于节点数据，遍历右子树，将值插入到右子树中
+                # 如果要插入的值大于等于节点数据，遍历右子树，将值插入到右子树中
                 if data >= tree_node._data:
                     if tree_node.right_node is None:
                         tree_node.right_node = new_node
                         new_node.parent_node = tree_node
                         return
+                    # 继续向下遍历
                     tree_node = tree_node.right_node
-                else: #要插入的值小于节点的数据，遍历左子树，将节点插入左子树
+                else:  # 要插入的值小于节点的数据，遍历左子树，将节点插入左子树
                     if tree_node.left_node is None:
                         tree_node.left_node = new_node
                         new_node.parent_node = tree_node
                         return
+                    # 继续向下遍历
                     tree_node = tree_node.left_node
 
-    def search(self,data):
+    def search(self, data):
         """
-        根据数据查找节点，如果又相同的数据都查出来
+        根据数据查找节点，如果有相同的数据都查出来
         :param data:
         :return:
         """
@@ -69,8 +73,7 @@ class BinarySearchTree(object):
                     tree_node = tree_node.left_node
         return res
 
-
-    def delete(self,data):
+    def delete(self, data):
         """
         找到数据，删除节点，如果又数据相同的节点都删除
         :param data:
@@ -80,21 +83,21 @@ class BinarySearchTree(object):
             return
         del_list = self.search(data)
         for node in del_list:
-            #父节点为空，又不是根节点，已经不在树上不需要删除
+            # 父节点为空，又不是根节点，已经不在树上不需要删除
             if node.parent_node is None and node != self.root:
                 continue
             else:
                 self._del(node)
 
-    def _del(self,node):
+    def _del(self, node):
         """
         删除操作
         :param node:
         :return:
         """
-        #如果要删除的节点没有子节点
+        # 如果要删除的节点没有子节点
         if node.left_node is None and node.right_node is None:
-            #要删除的是根节点
+            # 要删除的是根节点
             if node == self.root:
                 self.root = None
             else:
@@ -103,7 +106,7 @@ class BinarySearchTree(object):
                 else:
                     node.parent_node.right_node = None
                 node.parent_node = None
-        #左节点不存在，右节点存在
+        # 左节点不存在，右节点存在
         elif node.left_node is None and node.right_node is not None:
             if node == self.root:
                 self.root = node.right_node
@@ -131,10 +134,10 @@ class BinarySearchTree(object):
                 node.left_node.parent_node = node.parent_node
                 node.parent_node = None
                 node.left_node = None
-        #左右节点都存在的情况
+        # 左右节点都存在的情况
         else:
             min_node = node.right_node
-            #找到右子树的最小值节点
+            # 找到右子树的最小值节点
             if min_node.left_node:
                 min_node = min_node.left_node
 
@@ -146,9 +149,7 @@ class BinarySearchTree(object):
                 self._del(min_node)
                 self._del(node)
 
-
-
-    def pre_order(self,root):
+    def pre_order(self, root):
         """
         前序遍历
         :param root:
@@ -156,11 +157,11 @@ class BinarySearchTree(object):
         """
         if root is None:
             return
-        print(root._data,end=",")
+        print(root._data, end=",")
         self.pre_order(root.left_node)
         self.pre_order(root.right_node)
 
-    def in_order(self,root):
+    def in_order(self, root):
         """
         中序遍历,顺序遍历出数据
         :param root:
@@ -172,7 +173,7 @@ class BinarySearchTree(object):
         print(root._data, end=",")
         self.in_order(root.right_node)
 
-    def post_order(self,root):
+    def post_order(self, root):
         """
         后序遍历
         :param root:
@@ -184,23 +185,45 @@ class BinarySearchTree(object):
         self.post_order(root.right_node)
         print(root._data, end=",")
 
+    def layer_order(self, root):
+        """
+        按层遍历
+        :param root:
+        :return:
+        """
+        res_list = []
+        if root is None:
+            return res_list
+        queue = [root]
+        while queue:
+            node_list = []
+            res = []
+            for node in queue:
+                res.append(node._data)
+                if node.left_node is not None:
+                    node_list.append(node.left_node)
+                if node.right_node is not None:
+                    node_list.append(node.right_node)
+            res_list.append(res)
+            queue = node_list
+        print(res_list)
+        return res_list
+
 
 if __name__ == '__main__':
     binary_search_tree = BinarySearchTree()
-    datas = [10,8,15,12,16,6,9,15,15]
+    datas = [10, 8, 15, 12, 16, 6, 9, 15, 15]
     for i in datas:
         binary_search_tree.add(i)
 
-    binary_search_tree.pre_order(binary_search_tree.root)
-    print("")
-    binary_search_tree.in_order(binary_search_tree.root)
-    print("")
-    binary_search_tree.post_order(binary_search_tree.root)
-    print("")
-    print(binary_search_tree.search(15))
-    binary_search_tree.delete(15)
-    binary_search_tree.in_order(binary_search_tree.root)
-    print("")
-
-
-
+    # binary_search_tree.pre_order(binary_search_tree.root)
+    # print("")
+    # binary_search_tree.in_order(binary_search_tree.root)
+    # print("")
+    # binary_search_tree.post_order(binary_search_tree.root)
+    # print("")
+    # print(binary_search_tree.search(15))
+    # binary_search_tree.delete(15)
+    # binary_search_tree.in_order(binary_search_tree.root)
+    # print("")
+    binary_search_tree.layer_order(binary_search_tree.root)
---
+++ leetcode/28.py
@@ -0,0 +1,68 @@
+# -*- coding: utf8 -*-
+
+"""
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+ 
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        def do(left, right):
+            if left is None and right is None: return True
+            if left is None or right is None or left.val != right.val: return False
+            return do(left.left, right.right) and do(left.right, right.left)
+
+        return do(root.left, root.right) if root is not None else True
+
+
+if __name__ == '__main__':
+    slt = Solution()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(4)
+    root.right.right = TreeNode(3)
+    print(slt.isSymmetric(root))
